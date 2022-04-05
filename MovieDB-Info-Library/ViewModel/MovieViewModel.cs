15a97e082bffdb3072d84559044f608e9b0c4a50
tXshcode <schauer04@gmail.com>
2022-04-05 08:08:20 +0200

    Update MovieViewModel

--- MovieDB-Info-Library/ViewModel/MovieViewModel.cs
+++ MovieDB-Info-Library/ViewModel/MovieViewModel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MovieDB_Info_Library.ViewModel;
+using MovieDB_Info_Library.API;
+using MovieDB_Info_Library.Model;
 
 namespace MovieDB_Info_Library.ViewModel
 {
@@ -10,8 +14,22 @@ namespace MovieDB_Info_Library.ViewModel
     {
         #region Declarations
 
-        
+        public ICommand CallCommand { get; set; }
+
+        string SearchTitle { get; set; }
+        Movie Result { get; set; }
 
         #endregion
-    }
+
+        public MovieViewModel()
+        {
+
+            CallCommand = new RelayCommand(e =>
+                {
+                    Result = Call.APIcall(SearchTitle);
+
+                }
+
+            );
+        }
 }
